d66678a6956293b242ef9c091547f66ab2d51279
mhmad.kavosi <mhmad.kavosi@gmail.com>
2020-04-05 22:28:32 +0430

    Finish part 5

--- pollapp-project/polls/views.py
+++ pollapp-project/polls/views.py
@@ -3,19 +3,22 @@ from django.http import HttpResponseRedirect
 from .models import Question , Choice
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
+
 
 class IndexView(generic.ListView):
     template_name = 'poll/index.html'
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'poll/detail.html'
-
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 class ResultsView(generic.DetailView):
     model = Question
