414396475c739b55d737ae6de8af801f020bbc6c
mhmad.kavosi <mhmad.kavosi@gmail.com>
2020-04-05 15:09:52 +0430

    Finish Part3

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ This website is course of https://docs.djangoproject.com/en/3.0/intro/ pollapp
 
 - [x] Part 1 <br>
 - [x] Part 2 <br>
-- [ ] Part 3 <br>
+- [x] Part 3 <br>
 - [ ] Part 4 <br>
 - [ ] Part 5 <br>
 - [ ] Part 6 <br>
--- pollapp-project/polls/views.py
+++ pollapp-project/polls/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.shortcuts import render,get_object_or_404
+from django.http import HttpResponse,Http404
+from .models import Question
 def index(request):
-    return HttpResponse("Hello , wordl. You're at polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list':latest_question_list}
+    return render(request,'poll/index.html',context)
+
+def detail(request,question_id):
+    question = get_object_or_404(Question,pk=question_id)
+    return render(request, 'poll/detail.html', {'question': question})
+    
+
+
+def result(request,question_id):
+    respons = 'You are loking at results of question %s'
+    return HttpResponse(respons % question_id)
+
+def vote(request,question_id):
+    return HttpResponse(f'You are voting question {question_id}')
